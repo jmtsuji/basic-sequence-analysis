e303d713fc0d013efd6dd16e8e00074122f4eca2
Jackson Tsuji <jackson.tsuji@uwaterloo.ca>
2017-11-19 12:52:58 -0500

    Added support for white spaces in the find/replace fields.

--- text_find_and_replace.sh
+++ text_find_and_replace.sh
@@ -10,12 +10,12 @@ set -e
 set -u
 set -o pipefail
 
-script_version=1.0.0
+script_version=1.1.0
 
 # If no input is provided, exit out and provide help
 if [ $# == 0 ]
     then
-    printf "\n$(basename $0): finds and replaces selected text in a file.\nVersion: ${script_version}\nContact Jackson M. Tsuji (jackson.tsuji@uwaterloo.ca) for bug reports or feature requests.\n\nUsage: $(basename $0) text_replacement_file.tsv input_file.txt output_file.txt\n\nUsage details:\n**Output file should not exist ahead of time, or else it will be overwritten.\ntext_replacement_file.tsv: tab-separated file with headers. Old names (target) in first column, and new names (replacement) in second column. Case sensitive.\n\n"
+    printf "\n$(basename $0): finds and replaces selected text in a file.\nVersion: ${script_version}\nContact Jackson M. Tsuji (jackson.tsuji@uwaterloo.ca) for bug reports or feature requests.\n\nUsage: $(basename $0) text_replacement_file.tsv input_file.txt output_file.txt\n\nUsage details:\n**Output file should not exist ahead of time, or else it will be overwritten.\ntext_replacement_file.tsv: tab-separated file with headers. Old names (target) in first column, and new names (replacement) in second column. Case sensitive.\nAVOID special characters (other than whitespace, for which support was added in v1.1.0)\n\n"
     exit 1
 fi
 # Using printf: http://stackoverflow.com/a/8467449 (accessed Feb 21, 2017)
@@ -37,10 +37,17 @@ echo "Replacing items in file $(basename ${input_name}), based on the list provi
 
 cd "${work_dir}"
 
+# Temporarily change the internal fields separator (IFS) so that whitespaces in find/replace scheme do not create new entries. See Vince Buffalo's "Bioinformatics Data Skills" (1st Ed.) chapter 12, pg 407 and corresponding Github page README at https://github.com/vsbuffalo/bds-files/tree/master/chapter-12-pipelines (accessed Nov 19, 2017)
+OFS="$IFS"
+IFS=$'\n'
+
 # Get lists of old and new filenames and store as arrays
-names_old=($(tail -n +2 $replacement_info | cut -d $'\t' -f 1))
-names_new=($(tail -n +2 $replacement_info | cut -d $'\t' -f 2))
+names_old=($(tail -n +2 "${replacement_info}" | cut -d $'\t' -f 1))
+names_new=($(tail -n +2 "${replacement_info}" | cut -d $'\t' -f 2))
 # Got help from http://stackoverflow.com/questions/2961673/find-missing-argument-to-exec, post by Marian on June 2, 2010; accessed May 13, 2016
+
+# Fix the IFS
+IFS="$OFS"
 
 # Get number of names
 num_names=${#names_old[@]}
@@ -48,7 +55,7 @@ num_names=${#names_old[@]}
 # Confirm number of names is equal between lists
 if [ ${#names_old[@]} != ${#names_new[@]} ]
 then
-    echo "Error: ${#names_old[@]} entires in old names file and ${#names_new[@]} in new names file. These do not match. This could possibly be due to white spaces in one of the files, which this program does not support. Exiting..."
+    echo "Error: ${#names_old[@]} entries in old names column and ${#names_new[@]} in new names column. These do not match. This could possibly be due to special characers in one of the columns, which this has limited support for. Exiting..."
     exit 1
 else echo "Identified ${num_names} items for replacement."
 fi
@@ -56,13 +63,13 @@ fi
 echo ""
 
 # Make a copy of the tree file for multiple in-place edits
-cp $input_name $output_name
+cp "${input_name}" "${output_name}"
 
 for i in $(seq 1 ${num_names})
 do
 # Get names
-    old_name=${names_old[i-1]}
-    new_name=${names_new[i-1]}
+    old_name="${names_old[i-1]}"
+    new_name="${names_new[i-1]}"
     echo "${old_name} --> ${new_name}"
     sed -i -e "s/${old_name}/${new_name}/g" $output_name
 done
