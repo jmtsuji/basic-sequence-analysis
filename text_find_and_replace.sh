317d2f1aae7fc360de3076f3a86448b21a17453d
Jackson M. Tsuji <jackson.tsuji@uwaterloo.ca>
2018-06-29 01:57:33 -0400

    Cleaned up code syntax

--- text_find_and_replace.sh
+++ text_find_and_replace.sh
@@ -6,7 +6,7 @@ set -euo pipefail
 # Created Nov. 18, 2017
 # Description: Finds and replaces target names in a file with user-provided names. Works for any text file (e.g., renaming FastA sequence headers, phylogenetic tree file names, and so on.
 
-script_version=1.1.1
+script_version=1.2.0
 
 # If no input is provided, exit out and provide help
 if [ $# == 0 ]; then
@@ -20,18 +20,17 @@ if [ $# == 0 ]; then
    	exit 1
 fi
 
-# set variables from user input:
+# Set variables from user arguments
 replacement_info=$1
 input_name=$2
 output_name=$3
 
 # By default, set working directory to present working directory (pwd)
+# TODO - delete this -- it is redundant.
 work_dir=$(pwd)
 
-echo "Running $(basename $0), version $script_version."
-echo ""
-echo "Replacing items in file $(basename ${input_name}), based on the list provided in $(basename ${replacement_info})."
-
+printf "Running $(basename $0), version $script_version.\n"
+printf "Replacing items in file $(basename ${input_name}), based on the list provided in $(basename ${replacement_info}).\n"
 
 cd "${work_dir}"
 
@@ -47,40 +46,40 @@ names_new=($(tail -n +2 "${replacement_info}" | cut -d $'\t' -f 2))
 # Fix the IFS
 IFS="$OFS"
 
-# Get number of names
-num_names=${#names_old[@]}
-
 # Confirm number of names is equal between lists
-if [ ${#names_old[@]} != ${#names_new[@]} ]
-then
-    echo "Error: ${#names_old[@]} entries in old names column and ${#names_new[@]} in new names column. These do not match. This could possibly be due to special characers in one of the columns, which this has limited support for. Exiting..."
-    exit 1
-else echo "Identified ${num_names} items for replacement."
-fi
+if [ ${#names_old[@]} != ${#names_new[@]} ]; then
 
-echo ""
+	printf "Error: ${#names_old[@]} entries in old names column and ${#names_new[@]} in new names column. These do not match. This could possibly be due to special characers in one of the columns, which this has limited support for. Exiting...\n"
+	exit 1
+
+else
+
+	printf "Identified ${#names_old[@]} items for replacement.\n\n"
+
+fi
 
 # Make a copy of the tree file for multiple in-place edits
 cp "${input_name}" "${output_name}"
 
-for i in $(seq 1 ${num_names})
-do
-# Get names
-    old_name="${names_old[i-1]}"
-    new_name="${names_new[i-1]}"
-    echo "${old_name} --> ${new_name}"
-    sed -i -e "s/${old_name}/${new_name}/g" $output_name
+for i in $(seq 1 ${num_names}); do
+
+	# Set counter to start from zero
+	j=$((${i}-1))
+
+	# Get names
+	old_name="${names_old[${j}]}"
+	new_name="${names_new[${j}]}"
+	echo "${old_name} --> ${new_name}"
+
+	# Run the replacement
+	sed -i -e "s/${old_name}/${new_name}/g" $output_name
+	# sed help from http://unix.stackexchange.com/a/159369 (main) and http://stackoverflow.com/a/15236526 (debugging), accessed Feb. 22, 2017
+
 done
-# "for" loop idea from Bioinformatics Data Skills (Vince Buffalo), Ch. 12
-# sed help from http://unix.stackexchange.com/a/159369 (main) and http://stackoverflow.com/a/15236526 (debugging), accessed Feb. 22, 2017
 
 # Remove backup file created by sed once done
 rm ${output_name}-e
 
+printf "\nReplacing finished. Output saved as $(basename ${output_name}).\n\n"
+printf "$(basename $0): finished.\n\n"
 
-echo ""
-echo "Replacing finished. Output saved as $(basename ${output_name})."
-echo ""
-
-echo "$(basename $0): finished."
-echo ""
