adb396abde75b37245be2636beb6b5d910a0d146
Jackson Tsuji <jackson.tsuji@uwaterloo.ca>
2019-10-24 13:50:34 -0400

    Updated version and logging

--- scripts/run_blastp_parallel.sh
+++ scripts/run_blastp_parallel.sh
@@ -6,7 +6,7 @@ set -euo pipefail
 # NOTE: script is still in development.
 
 # GLOBAL VARIABLES
-SCRIPT_VERSION="0.0.1"
+VERSION=$(basic-sequence-analysis-version)
 SCRIPT_NAME=${0##*/}
 
 # PRESETS - set these up for your server!
@@ -18,12 +18,12 @@ LINEAGE_REPO_LOCATION="/Data/reference_databases/blast/2018-ncbi-lineages"
 
 # If incorrect input is provided, provide help and exit
 if [ $# -ne 1 -a $# -ne 5 ]; then
-  printf "Error: missing or extra arguments supplied. To see help statement, run '${SCRIPT_NAME} -h'\n"
+  printf "Error: missing or extra arguments supplied. To see help statement, run '${SCRIPT_NAME} -h'\n" >&2
   exit 1
 elif [ $1 = "-h" -o $1 = "--help" ]; then
   # Help statement
   printf "${SCRIPT_NAME}: Runs BLASTP in parallel to find closest hits for given queries. Also outputs taxonomy.\n"
-  printf "Version: ${SCRIPT_VERSION}\n"
+  printf "Version: ${VERSION}\n"
   printf "Copyright Jackson M. Tsuji, Neufeld Research Group, 2019\n"
   printf "Dependencies: BLAST+\n\n"
   printf "Usage: ${SCRIPT_NAME} query_faa output_tsv evalue max_targets threads\n\n"
@@ -48,40 +48,40 @@ output_basepath="${output_tmpdir}/blastp_tmp"
 
 # Check inputs
 if [[ ! -f "${query_filepath}" ]]; then
-  echo "[ $(date -u) ]: ERROR: input file not found at '${query_filepath}'. Exiting..."
+  echo "[ $(date -u) ]: ERROR: input file not found at '${query_filepath}'. Exiting..." >&2
   exit 1
 elif [[ "${query_filepath##*.}" != "faa" ]]; then
-  echo "[ $(date -u) ]: WARNING: extension of input file is '${query_filepath##*.}' (expecteding 'faa')"
+  echo "[ $(date -u) ]: WARNING: extension of input file is '${query_filepath##*.}' (expecteding 'faa')" >&2
 fi
 
 # Check outputs
 if [[ -f "${output_filepath}" ]]; then
-  echo "[ $(date -u) ]: ERROR: another file is already present at desired output filepath '${output_filepath}'. Refusing to continue..."
+  echo "[ $(date -u) ]: ERROR: another file is already present at desired output filepath '${output_filepath}'. Refusing to continue..." >&2
   exit 1
 elif [[ ! -d "${output_filepath%/*}" ]]; then
-  echo "[ $(date -u) ]: ERROR: desired directory for output file '${output_filepath}' does not exist. Please make this folder. Exiting..."
+  echo "[ $(date -u) ]: ERROR: desired directory for output file '${output_filepath}' does not exist. Please make this folder. Exiting..." >&2
   exit 1
 elif [[ -d "${output_tmpdir}" ]]; then
-  echo "[ $(date -u) ]: ERROR: tmp output directory '${output_tmpdir}' already exists. Refusing to continue..."
+  echo "[ $(date -u) ]: ERROR: tmp output directory '${output_tmpdir}' already exists. Refusing to continue..." >&2
   exit 1
 fi
 
 # Startup messages
-echo "[ $(date -u) ]: Running ${SCRIPT_NAME}"
-echo "[ $(date -u) ]: Version: ${SCRIPT_VERSION}"
-echo "[ $(date -u) ]: Command run: ${SCRIPT_NAME} ${@}"
-echo "[ $(date -u) ]: ######## PRESETS ########"
-echo "[ $(date -u) ]: DB_NAME: '${DB_NAME}'"
-echo "[ $(date -u) ]: BLASTDB: '${BLASTDB}'"
-echo "[ $(date -u) ]: TAXDUMP_LOCATION: '${TAXDUMP_LOCATION}'"
-echo "[ $(date -u) ]: LINEAGE_REPO_LOCATION: '${LINEAGE_REPO_LOCATION}'"
-echo "[ $(date -u) ]: ##### USER SETTINGS #####"
-echo "[ $(date -u) ]: query_filepath: '${query_filepath}'"
-echo "[ $(date -u) ]: output_filepath: '${output_filepath}'"
-echo "[ $(date -u) ]: evalue: '${evalue}'"
-echo "[ $(date -u) ]: max_target_seqs: '${max_target_seqs}'"
-echo "[ $(date -u) ]: threads: ${threads}"
-echo "[ $(date -u) ]: #########################"
+echo "[ $(date -u) ]: Running ${SCRIPT_NAME}" >&2
+echo "[ $(date -u) ]: Version: ${VERSION}" >&2
+echo "[ $(date -u) ]: Command run: ${SCRIPT_NAME} ${@}" >&2
+echo "[ $(date -u) ]: ######## PRESETS ########" >&2
+echo "[ $(date -u) ]: DB_NAME: '${DB_NAME}'" >&2
+echo "[ $(date -u) ]: BLASTDB: '${BLASTDB}'" >&2
+echo "[ $(date -u) ]: TAXDUMP_LOCATION: '${TAXDUMP_LOCATION}'" >&2
+echo "[ $(date -u) ]: LINEAGE_REPO_LOCATION: '${LINEAGE_REPO_LOCATION}'" >&2
+echo "[ $(date -u) ]: ##### USER SETTINGS #####" >&2
+echo "[ $(date -u) ]: query_filepath: '${query_filepath}'" >&2
+echo "[ $(date -u) ]: output_filepath: '${output_filepath}'" >&2
+echo "[ $(date -u) ]: evalue: '${evalue}'" >&2
+echo "[ $(date -u) ]: max_target_seqs: '${max_target_seqs}'" >&2
+echo "[ $(date -u) ]: threads: ${threads}" >&2
+echo "[ $(date -u) ]: #########################" >&2
 
 # Make tmp dir
 mkdir ${output_tmpdir}
@@ -92,7 +92,7 @@ echo "[BLAST]" > .ncbirc
 echo "BLASTDB=\"${BLASTDB}\"" >> .ncbirc
 
 # Run BLAST in parallel; idea from https://www.biostars.org/p/63816/ (accessed 190306)
-echo "[ $(date -u) ]: Running BLAST on ${threads} threads (will take time)"
+echo "[ $(date -u) ]: Running BLAST on ${threads} threads (will take time)" >&2
 cat "${query_filepath}" | \
   parallel -q -j ${threads} -k --block 1k --recstart '>' --pipe \
   blastp -db ${DB_NAME} -query - -evalue ${evalue} -max_target_seqs ${max_target_seqs} -num_threads 1 \
@@ -100,7 +100,7 @@ cat "${query_filepath}" | \
   ${output_basepath}_raw.tsv
 
 # Add header
-echo "[ $(date -u) ]: Performing intermediate file prep"
+echo "[ $(date -u) ]: Performing intermediate file prep" >&2
 sed -i '1 i\qseqid\tsseqid\tpident\tevalue\tqcovhsp\tbitscore\tstaxid\tsscinamet\stitle' ${output_basepath}_raw.tsv
 
 # Get full taxonomic lineage (beta code from C. Titus Brown's group)
@@ -113,7 +113,7 @@ paste -d ',' ${output_basepath}_taxids.tmp.1 ${output_basepath}_taxids.tmp.2 > $
 rm ${output_basepath}_taxids.tmp.1 ${output_basepath}_taxids.tmp.2
 
 # Run the lineage code
-echo "[ $(date -u) ]: Getting NCBI linages (don't be surprised if a small number of errors are printed below; usually a few taxid's don't match the taxdump)"
+echo "[ $(date -u) ]: Getting NCBI linages (don't be surprised if a small number of errors are printed below; usually a few taxid's don't match the taxdump)" >&2
 ${LINEAGE_REPO_LOCATION}/make-lineage-csv.py \
   ${TAXDUMP_LOCATION}/nodes.dmp \
   ${TAXDUMP_LOCATION}/names.dmp \
@@ -130,12 +130,12 @@ rm ${output_basepath}_taxids.csv \
 # Note that a few taxid's seem to not match most of the time. I think this must be due to the taxdump and the blast taxonomy info not being 100% in sync
 
 # Confirm the results still match
-echo "[ $(date -u) ]: Checking and combining outputs"
+echo "[ $(date -u) ]: Checking and combining outputs" >&2
 tail -n +2 ${output_basepath}_raw.tsv | cut -d $'\t' -f 2 | cut -d '|' -f 4 > ${output_basepath}_taxids.tmp.3
 tail -n +2 ${output_basepath}_lineages.tsv | cut -d $'\t' -f 1 > ${output_basepath}_taxids.tmp.4
 output_status=$(cmp ${output_basepath}_taxids.tmp.3 ${output_basepath}_taxids.tmp.4 </dev/null; echo $?)
 if [ ${output_status} != 0 ]; then
-  echo "[ $(date -u) ]: ERROR: lineages table does not match the order of the original BLAST results. Cannot continue. Exiting..."
+  echo "[ $(date -u) ]: ERROR: lineages table does not match the order of the original BLAST results. Cannot continue. Exiting..." >&2
   exit 1
 fi
 
@@ -143,7 +143,7 @@ fi
 rm ${output_basepath}_taxids.tmp.3 ${output_basepath}_taxids.tmp.4
 
 # Combine the tables
-echo "[ $(date -u) ]: Writing output"
+echo "[ $(date -u) ]: Writing output" >&2
 cut -d $'\t' -f 3- ${output_basepath}_lineages.tsv | paste -d $'\t' ${output_basepath}_raw.tsv /dev/stdin > ${output_filepath}
 
 # Clean up
@@ -152,4 +152,4 @@ rm ${output_basepath}_lineages.tsv \
   .ncbirc
 rmdir ${output_tmpdir} # will throw an error if any extra files are still left in the folder
 
-echo "[ $(date -u) ]: ${SCRIPT_NAME}: finished."
+echo "[ $(date -u) ]: ${SCRIPT_NAME}: finished." >&2
